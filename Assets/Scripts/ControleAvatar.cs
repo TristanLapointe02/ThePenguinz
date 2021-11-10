b4bf17e8b72ffda4314116652f5a44638bb8e320
JeremyEmondLapierre <72357541+JeremyEmondLapierre@users.noreply.github.com>
2021-11-10 10:27:06 -0500

    Modification déplacement main en multijoueur

--- Assets/Scripts/ControleAvatar.cs
+++ Assets/Scripts/ControleAvatar.cs
@@ -16,10 +16,15 @@ public class ControleAvatar : MonoBehaviour
     public Transform AvatarTransformPrincipal;
     public Transform AvatarTete;
     public Transform AvatarCorps;
+    public Transform AvatarMainGauche;
+    public Transform AvatarMainDroite;
+    
 
 
     //Transforms du XR Rig qui est la tête (normalement, c'est la caméra)
     public Transform XRTete;
+    public Transform XRLeftHand;
+    public Transform XRRightHand;
 
     //Distance entre la position du coprs et la tête (environ -0.9 dans l'exemple du cours)
     public Vector3 tetePositionOffset;
@@ -34,5 +39,7 @@ public class ControleAvatar : MonoBehaviour
         AvatarTransformPrincipal.position = Vector3.Lerp(AvatarTransformPrincipal.position, XRTete.position + tetePositionOffset, 0.5f);
         AvatarTete.rotation = Quaternion.Lerp(AvatarTete.rotation, XRTete.rotation, 0.5f);
         AvatarCorps.rotation = Quaternion.Lerp(AvatarCorps.rotation, Quaternion.Euler(new Vector3(0, AvatarTete.rotation.eulerAngles.y, 0)), 0.05f);
+        AvatarMainDroite.position = Vector3.Lerp(AvatarMainDroite.position, XRRightHand.position, 0.5f);
+        AvatarMainGauche.position = Vector3.Lerp(AvatarMainGauche.position, XRLeftHand.position, 0.5f);
     }
 }
