aca1bf722e570298a2fe80844225bb4a1e276812
JeremyEmondLapierre <72357541+JeremyEmondLapierre@users.noreply.github.com>
2021-11-17 11:19:59 -0500

    Tir avec gachette

--- Assets/Scripts/tirBalle.cs
+++ Assets/Scripts/tirBalle.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Realtime;
 using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR.Interaction.Toolkit; // accès aux objets du XR Interaction Toolkit
+using UnityEngine.InputSystem; // pour utiliser le nouveau InputSyteme
+using UnityEngine.Events;
 
 public class tirBalle : MonoBehaviourPunCallbacks
 {
     public bool peutTirer = true; //V�rifie si le joueur peut tirer
     public GameObject balle; //La balle qui est instanci�e au tir
     public GameObject boutFusil; //D�termine o� est le bout du fusil (o� la balle devrait spawn)
+
+    // L'action du contrôleur qui active/désactive le rayon. Peut être autre chose que le grip. Action à définir dans le tableau InputAction
+    [SerializeField]
+    InputActionReference inputActionReference_ActiveTrigger; 
 
     private void Start()
     {
@@ -28,6 +37,36 @@ public class tirBalle : MonoBehaviourPunCallbacks
                 //Lui appliquer une v�locit� pour la projeter vers l'avant
                 nouvelleBalle.GetComponent<Rigidbody>().velocity = boutFusil.transform.forward * 40;
             }
+
+        }
+    }
+
+    private void OnEnable(){
+        inputActionReference_ActiveTrigger.action.performed += ActiveTrigger;
+    }
+
+    private void OnDisable()
+    {
+        // s'exécute lorsque le script devient inactif (disable)
+        // retire la fonction qui sera appelée lorsque l'action sera effectuée
+        inputActionReference_ActiveTrigger.action.performed -= ActiveTrigger;
+    }
+
+    private void ActiveTrigger(InputAction.CallbackContext obj)
+    {
+       
+        // Bouton enfoncé, on active le rayon
+        // ReadValue<float> permet de récuperé la valeur de type float contenu dans le paramètre obj
+        if (obj.ReadValue<float>() == 1f)
+        {
+            if (photonView.IsMine){
+                GameObject nouvelleBalle = PhotonNetwork.Instantiate(balle.name, boutFusil.transform.position, boutFusil.transform.rotation, 0, null);
+                //Lui appliquer une v�locit� pour la projeter vers l'avant
+                nouvelleBalle.GetComponent<Rigidbody>().velocity = boutFusil.transform.forward * 40;
+                print("TEST");
+            }
+            
         }
     }
 }
+
